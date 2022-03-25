da5c67ecc26139157dbc795cac78b54c82eb38ed
Danny Warren <danny@theforbiddenarts.com>
2022-03-24 21:42:43 -0400

    code submition

--- HomeworkArrays/Program.cs
+++ HomeworkArrays/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+string[] names = new string[] { "Tim", "John", "Sue" };
+int selection;
+bool validNum;
+
+do
+{
+    Console.Write("Please choose a number from 1 to 3: ");
+    string? selectionText = Console.ReadLine();
+    validNum = Int32.TryParse(selectionText, out selection);
+
+    if (selection < 1 || selection > 3)
+    {
+        Console.WriteLine("You have entered an invalid option. Please try again.");
+        validNum = false;
+    }
+
+} while (validNum != true);
+
+Console.WriteLine($"You have chosen {names[selection - 1]}");
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 HomeworkArraysApp
+
+Create an array of 3 names.
+Ask the user which number to select.
+When the user gives a number, return that name.
+Make sure to check for invalid numbers.
